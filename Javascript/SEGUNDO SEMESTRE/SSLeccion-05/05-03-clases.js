151ee96afcfd0b2eee66455bae357195593a52d0
Franco Poblete <168313957+carfrank84@users.noreply.github.com>
2024-10-09 08:45:23 -0300

    Clase 6 Clases Parte 1
    sintaxis de clases , metodo get y set , hosting, y herencia

---
+++ Javascript/SEGUNDO SEMESTRE/SSLeccion-05/05-03-clases.js
@@ -0,0 +1,56 @@
+//let persona3 = new Persona('Carla', 'Popnce'); esto no se debe hacer : Persona is not defined
+
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre (){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+class empleado extends Persona{// clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+}
+
+
+let persona1 = new Persona('Martin','Perez' );
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.nombre = 'Juan Carlos';
+persona1.apellido = 'Poblete';
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+//console.log(persona1)
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+persona2.nombre = 'Maria Laura';
+persona2.apellido = 'Moyano';
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+//console.log(persona2);
+
+let empleado1 = new empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre);
